80896e44d01dc69c4c2e9dd39a2eca60e65dd00c
Sahil Rupani <sahilrupani@monitree.co>
2025-09-25 15:20:08 +0530

    updated common script

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -20,6 +20,30 @@ if [ ! -f "$KAFKA_UI_JAR_PATH" ]; then
   fi
 fi
 
+# Map shared security env to Kafka UI expected vars if not explicitly set
+if [ -n "${KAFKA_SECURITY_PROTOCOL:-}" ] && [ -z "${KAFKA_CLUSTERS_0_PROPERTIES_SECURITY_PROTOCOL:-}" ]; then
+  export KAFKA_CLUSTERS_0_PROPERTIES_SECURITY_PROTOCOL="$KAFKA_SECURITY_PROTOCOL"
+fi
+
+if [ -n "${KAFKA_SASL_MECHANISM:-}" ] && [ -z "${KAFKA_CLUSTERS_0_PROPERTIES_SASL_MECHANISM:-}" ]; then
+  export KAFKA_CLUSTERS_0_PROPERTIES_SASL_MECHANISM="$KAFKA_SASL_MECHANISM"
+fi
+
+# If username/password provided and JAAS not set, synthesize JAAS config for PLAIN/SCRAM
+if [ -n "${KAFKA_SASL_USERNAME:-}" ] && [ -n "${KAFKA_SASL_PASSWORD:-}" ] && [ -z "${KAFKA_CLUSTERS_0_PROPERTIES_SASL_JAAS_CONFIG:-}" ]; then
+  export KAFKA_CLUSTERS_0_PROPERTIES_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${KAFKA_SASL_USERNAME}\" password=\"${KAFKA_SASL_PASSWORD}\";"
+fi
+
+# If KAFKA_SSL=true set SSL if protocol not set
+if [ "${KAFKA_SSL:-}" = "true" ] && [ -z "${KAFKA_CLUSTERS_0_PROPERTIES_SECURITY_PROTOCOL:-}" ]; then
+  export KAFKA_CLUSTERS_0_PROPERTIES_SECURITY_PROTOCOL="SSL"
+fi
+
+# Helpful defaults for SSL name verification
+if [ -z "${KAFKA_CLUSTERS_0_PROPERTIES_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM:-}" ]; then
+  export KAFKA_CLUSTERS_0_PROPERTIES_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM="https"
+fi
+
 # Start producer
 PORT="$PRODUCER_PORT" node /app/producer/src/index.js &
 PID_PRODUCER=$!
@@ -33,4 +57,3 @@ echo "[all-in-one] Consumer PID: $PID_CONSUMER"
 # Start Kafka UI in foreground
 echo "[all-in-one] Launching Kafka UI ..."
 exec java -jar "$KAFKA_UI_JAR_PATH"
-
